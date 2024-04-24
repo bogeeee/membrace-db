7a3d18f5e5ead73af198e05d65d5d10bec75c809
Boris Gingold <bogeee@bogitech.de>
2024-04-24 23:08:09 +0200

    Tests

--- mini-db/index.test.ts
+++ mini-db/index.test.ts
@@ -1,12 +1,40 @@
 import * as fs from "node:fs";
 import {it, expect, test, beforeEach,describe } from 'vitest'
 import {MiniDb} from "./index";
+import exp from "constants";
 
 
 beforeEach(() => {
+    // Clear folder:
     if(fs.existsSync("db")) {
-        fs.rmdirSync("db");
+        fs.rmdirSync("db", {recursive: true});
     }
+});
+
+function createSampleObjectGraphForJson() {
+    return {
+        appName: "HelloApp",
+        users: [{id: 1, name: "Bob", active: true}, {id: 2, name: "Alice", active: false}],
+        nullable: null,
+    }
+}
+
+
+describe('Basic test', () => {
+    it("should have the same graph when reopening", () => {
+        let miniDb = new MiniDb("db", {root: createSampleObjectGraphForJson()});
+        miniDb.close();
+
+
+        let miniDb2 = new MiniDb("db");
+        try {
+            expect(miniDb2.root).toStrictEqual(createSampleObjectGraphForJson());
+        }
+        finally {
+            miniDb2.close();
+        }
+    })
+
 });
 
 describe('db locking', () => {
